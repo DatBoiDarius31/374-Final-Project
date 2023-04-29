c9f08747a1f81b2300d05d2cbf987ad13e3df4b5
DatBoiDarius31 <91650708+DatBoiDarius31@users.noreply.github.com>
2023-04-29 12:13:04 -0500

    added (unfinished) enrollment html and php
    added the files for the enrollment and php files, have not connect to or made post method will modify code in php file

---
+++ enrollmentData.php
@@ -0,0 +1,51 @@
+<?php
+$page_title = 'Enroll';
+include('header.html');
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        require('connect.php');
+        $errors = array();
+
+        $un = mysqli_real_escape_string($link, trim($_POST['username']));
+
+        $fn = mysqli_real_escape_string($link, trim($_POST['firstname']));
+
+        $ln = mysqli_real_escape_string($link, trim($_POST['lastname']));
+
+        $e = mysqli_real_escape_string($link, trim($_POST['phone']));
+
+        $p = mysqli_real_escape_string($link, trim($_POST['password1']));
+    }
+
+
+
+    if(empty($errors))
+    {
+        $q = "INSERT INTO Members (username,first_name, last_name, phone, password, date_joined) VALUES ('$un','$fn','$ln','$e','$p',NOW())";
+        $r = @mysqli_query($link,$q);
+        echo var_dump($r);
+
+        if($r)
+        {
+            echo '<h1>Registered!</h1><p>You are now registered.</p><p><a href="login.php">Login</a></p>';
+        }
+        ?>
+        <?php
+        mysqli_close($link);
+
+        include('includes/footer.html');
+        exit();
+    }
+
+    else
+    {
+        ?>
+        <?php
+        echo '<h1>Error!</h1><p id="err_msg">The folowing error(s) occurred:<br>';
+        foreach($errors as $msg)
+        {echo " - $msg<br>";}
+
+        echo'Please try again.</p>';
+        mysqli_close($link);
+    }
