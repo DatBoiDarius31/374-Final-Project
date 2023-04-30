db761bbcafde008e2915cbbab3f96d0cbc2cd9bf
DatBoiDarius31 <91650708+DatBoiDarius31@users.noreply.github.com>
2023-04-30 10:22:11 -0500

    debug

--- enrollmentData.php
+++ enrollmentData.php
@@ -1,75 +1,57 @@
 <?php
 $page_title = 'Enroll';
 include('header.html');
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
 
-require('connect.php');
-        $errors = array();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require('connect.php');
+    $errors = array();
 
+    // Get form data
+    session_start();
+    $member_id = $_SESSION["id"];
+    $programName = $_POST["programName"];
 
-      
+    if ($programName == "Tiny Tigers") {
+        $payment_id = 1;
+        $amount = 70.00;
+        $payment_date = date("m-d-Y");
+    } else if ($programName == "Little Ninjas") {
+        $payment_id = 2;
+        $amount = 100.00;
+        $payment_date = date("m-d-Y");
+    } else if ($programName == "Junior") {
+        $payment_id = 3;
+        $amount = 120.00;
+        $payment_date = date("m-d-Y");
+    } else if ($programName == "Defense and Tactical Training Program") {
+        $payment_id = 4;
+        $amount = 140.00;
+        $payment_date = date("m-d-Y");
+    }
 
+    $instructor_id = $_POST["instructors_id"];
 
+    if (empty($errors)) {
+        // Insert data into MySQL table
+        $q = "INSERT INTO Enrollments (member_id, payment_id, programName, instructor_id) VALUES ('$member_id', '$payment_id', '$programName', '$instructor_id');";
+        $r = @mysqli_query($link, $q);
 
-// Get form data
-$member_id = $_SESSION["id"];
-$programName = $_POST["programName"];
+        $q2 = "INSERT INTO Payments (amount, member_id, payment_date) VALUES ('$amount', '$member_id', '$payment_date');";
+        $r2 = @mysqli_query($link, $q2);
 
-if($programName == "Tiny Tigers"){
-    $payment_id = 1;
-    $amount = 70.00;
-    $payment_date = date("m-d-Y");
-}
-else if($programName == "Little Ninjas"){
-    $payment_id = 2;
-    $amount = 100.00;
-    $payment_date = date("m-d-Y");
-    
-}
-else if($programName == "Junior"){
-    $payment_id = 3;
-    $amount = 120.00;
-    $payment_date = date("m-d-Y");
-}
-else if($programName == "Defense and Tactical Training Program"){
-    $payment_id = 4;
-    $amount = 170.00;
-    $payment_date = date("m-d-Y");
-}
-
-$instructor_id = $_POST["instructor_id"];
-}
-
-if(empty($errors)){
-// Insert data into MySQL table
-$q = "INSERT INTO Enrollments (member_id, payment_id, programName, instructor_id) VALUES ('$member_id', '$payment_id', '$programName', '$instructor_id');
-INSERT INTO Payments (amount,member_id,payment_date) VALUES ('$payment_id','$member_id','$payment_date')";
-$r = @mysqli_query($link,$q);
-echo var_dump($r);
-
-if($r || $r2)
-        {
+        if ($r && $r2) {
             echo '<h1>Enrolled</h1><p>You are now Enrolled.</p><p><a href="members.php">Members</a></p>';
+        } else {
+            echo '<p>' . mysqli_error($link) . '</p>';
         }
-        ?>
-        <?php
+
         mysqli_close($link);
 
         include('includes/footer.html');
         exit();
-}
-else
-    {
-        ?>
-        <?php
-        echo '<h1>Error!</h1><p id="err_msg">The following error(s) occurred:<br>';
-        foreach($errors as $msg)
-        {echo " - $msg<br>";}
-
-        echo'Please try again.</p>';
-        mysqli_close($link);
     }
-?>
+} else {
+    echo '<p class="error">This page has been accessed in error.</p>';
+}
 
-
+include('includes/footer.html');
